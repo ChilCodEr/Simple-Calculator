975d010eb0215635fa229579db07c7e5d05722c3
Chill Coder <108239398+ChilCodEr@users.noreply.github.com>
2023-01-01 15:17:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+print("--------Addition------------")
+
+a = input("first number?  ")
+b = input("second number?  ")
+
+if (a).isnumeric() and (b).isnumeric():
+  print("answer: ", int(a) + int(b))
+  print("----------------------------")
+else:
+  print("--------------------------")
+  print("Please enter valid digits!")
+  print("--------Addition----------")
+
+  a = input("first number?  ")
+  b = input("second number?  ")
+  print("answer: ", int(a) + int(b))
+
+#========================================================================#
+
+print("                            ")
+
+print("--------Subtraction---------")
+
+c = input("first number?  ")
+d = input("second number?  ")
+
+if (c).isnumeric() and (d).isnumeric():
+  print("answer: ", int(c) - int(d))
+  print("----------------------------")
+else:
+  print("--------------------------")
+  print("Please enter valid digits!")
+  print("--------Subtraction-------")
+
+  c = input("first number?  ")
+  d = input("second number?  ")
+  print("answer: ", int(a) - int(b))
+
+#========================================================================#
+
+print("                            ")
+
+print("--------Multiplication------")
+
+e = input("first number?  ")
+f = input("second number?  ")
+
+if (e).isnumeric() and (f).isnumeric():
+  print("answer: ", int(e) * int(f))
+  print("----------------------------")
+else:
+  print("--------------------------")
+  print("Please enter valid digits!")
+  print("--------Multiplication----")
+
+  e = input("first number?  ")
+  f = input("second number?  ")
+  print("answer: ", int(a) * int(b))
+
+#========================================================================#
+
+print("                            ")
+
+print("--------Division------------")
+
+g = input("first number?  ")
+h = input("second number?  ")
+
+if (g).isnumeric() and (h).isnumeric():
+  print("answer: ", int(g) / int(h))
+  print("----------------------------")
+else:
+  print("--------------------------")
+  print("Please enter valid digits!")
+  print("--------Division----------")
+  
+  g = input("first number?  ")
+  h = input("second number?  ")
+  print("answer: ", int(a) / int(b))
+
